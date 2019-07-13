29189e0e00ab79ab27a630fcd16efe59384808ae
mrvrozt <48779035+mrvrozt@users.noreply.github.com>
2019-07-13 14:35:14 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,10 @@
 const Discord = require('discord.js');
 
 const client = new Discord.Client();
+
 client.on('ready', () => {
 console.log(`Logged in as ${client.user.tag}!`);
-client.user.setActivity("with Bobbo")
+client.user.setActivity("with Bobbo", { type: "PLAYING"}).catch(console.error);
 });
 
 
