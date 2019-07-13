060ca41136235bc34c1019adeb4dd44d0fb5cdfa
mrvrozt <48779035+mrvrozt@users.noreply.github.com>
2019-07-13 15:36:25 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,12 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
+//Type
+//0: playing
+//1: streaming
+//2: listening
+//3: watching
+
 client.on('ready', () => {
 client.user.setPresence({ game: { name: `with Bobbo's belly`, type: 0 } });
 });
