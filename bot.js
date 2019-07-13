72cfc83a17c724da33c164ee38a0d4e9755400eb
mrvrozt <48779035+mrvrozt@users.noreply.github.com>
2019-07-13 14:50:16 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
-cclient.on('ready', () => {
+client.on('ready', () => {
 console.log(`Logged in as ${client.user.tag}!`);
 
     client.user.setPresence({
