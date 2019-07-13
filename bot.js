02e48c18abf5f485688afcd34dc8ffe04b41fea9
mrvrozt <48779035+mrvrozt@users.noreply.github.com>
2019-07-13 14:52:24 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,13 +3,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-console.log(`Logged in as ${client.user.tag}!`);
-
-    client.user.setPresence({
-        game: {
-	      name: 'Seaside Fashionista',
-            type: "LISTENING",
-        }
-    })
+client.user.setActivity('yourGameNameHere', { type: 'PLAYING' });
  });
 client.login(process.env.BOT_TOKEN);
