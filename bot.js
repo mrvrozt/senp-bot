9a9563ba6483d0fc121866d832fde18f0ea45c47
mrvrozt <48779035+mrvrozt@users.noreply.github.com>
2019-07-13 14:57:08 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,6 +3,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-client.user.setPresence({ game: { name: 'nameGoesHere', type: 0 } });
+client.user.setPresence({ game: { name: `with Bobbo's belly`, type: 0 } });
 });
 client.login(process.env.BOT_TOKEN);
